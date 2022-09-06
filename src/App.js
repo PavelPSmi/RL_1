b31a25ad8d7ecac3868c1d653fb131b0e0fbcb96
PavelSP <SmirnovPavel1988@yandex.ru>
2022-09-06 19:36:51 +0300

    aaaaaa

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Первый урок по React, {props.name}</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
